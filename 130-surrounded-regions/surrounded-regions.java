2596f6cfe6db4a034b82e1437d5ea0a1bb92c169
PratikpatilCodeJourney <144219205+PratikpatilCodeJourney@users.noreply.github.com>
2025-02-26 08:55:12 +0530

    Time: 2 ms (86.29%) | Memory: 45.6 MB (48.04%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // 1. Check boundaries and mark safe regions
+        for (int i = 0; i < rows; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+            if (board[i][cols - 1] == 'O') {
+                dfs(board, i, cols - 1);
+            }
+        }
+        for (int j = 0; j < cols; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
+            }
+            if (board[rows - 1][j] == 'O') {
+                dfs(board, rows - 1, j);
+            }
+        }
+
+        // 2. Flip unsafe regions and restore safe ones
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = '#'; // Mark as visited (safe)
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
